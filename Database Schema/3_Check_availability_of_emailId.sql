4d40c01ca719cf1c391ad6da63a40ed6b26f6f55
bhavdeep33 <114081898+bhavdeep33@users.noreply.github.com>
2024-06-25 12:29:45 +0000

    Uploaded project files

---
+++ Database Schema/3_Check_availability_of_emailId.sql
@@ -0,0 +1,21 @@
+DELIMITER $$
+
+USE thereadhub;
+CREATE PROCEDURE prc_check_emailid_availability(
+    IN inputEmailId VARCHAR(254)
+)
+BEGIN 
+    SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
+    
+    START TRANSACTION;
+
+    SELECT EXISTS(
+        SELECT TRUE
+        FROM user_details
+        WHERE EmailId = inputEmailId
+    )AS alreadyRegistered;
+
+    COMMIT;
+END$$
+
+DELIMITER ;
